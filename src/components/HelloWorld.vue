47f3c7165612815578c73147838e976b8c038461
李玉辉 <liyuhui949@gmail.com>
2026-05-11 09:56:41 +0800

    feat: 安装mitt实现时间通信

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,3 +1,11 @@
+<!--
+ * @Author: 李玉辉 liyuhui949@gmail.com
+ * @Date: 2026-05-09 17:00:35
+ * @LastEditors: 李玉辉 liyuhui949@gmail.com
+ * @LastEditTime: 2026-05-09 17:30:30
+ * @FilePath: \my-component-library\src\components\HelloWorld.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
 <script setup lang="ts">
 import { ref } from 'vue'
 import viteLogo from '../assets/vite.svg'
