65011355fce8810801de1826efd1aa2ef801e4ef
李玉辉 <liyuhui949@gmail.com>
2026-05-11 10:02:41 +0800

    Revert "feat: 安装mitt实现时间通信"
    This reverts commit 47f3c7165612815578c73147838e976b8c038461.

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,6 @@
-<!--
- * @Author: 李玉辉 liyuhui949@gmail.com
- * @Date: 2026-05-09 17:18:00
- * @LastEditors: 李玉辉 liyuhui949@gmail.com
- * @LastEditTime: 2026-05-11 09:03:27
- * @FilePath: \my-component-library\src\App.vue
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
--->
-<script setup lang="ts"></script>
+<script setup lang="ts">
+</script>
 
 <template>
-  <keep-alive>
-    <router-view />
-  </keep-alive>
+  <router-view />
 </template>
