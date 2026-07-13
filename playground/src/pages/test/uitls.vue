6cadb397740444cfa68005c3476619e9fb05c674
@Li-yuhui0901 <liyuhui949@gmail.com>
2026-07-13 15:30:52 +0800

    docs：文档更新

--- playground/src/pages/test/uitls.vue
+++ playground/src/pages/test/uitls.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: 李玉辉
  * @Date: 2026-07-10 15:21:17
- * @LastEditTime: 2026-07-10 15:49:47
+ * @LastEditTime: 2026-07-13 10:57:24
  * @LastEditors: 李玉辉
  * @Description: 
 -->
@@ -24,6 +24,15 @@ console.log(c, obtainPreciseType(c)); // 输出: "object"
 console.log(d, obtainPreciseType(d)); // 输出: "array"
 console.log(e, obtainPreciseType(e)); // 输出: "number"
 console.log(f, obtainPreciseType(f)); // 输出: "boolean"
+
+const str = "Hello, World!";
+console.log(str, str.split(" "));
+
+const getNum = (num: number): number => {
+  return num % 7;
+};
+
+console.log(getNum(1000));
 </script>
 
 <template>
